47f74721296cde10252a5a8961f7fa15376349a3
Drew Coker <drewrosecoker@gmail.com>
2023-01-10 08:05:32 -0500

    refactored update

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-(function (){
-    "use strict";
-    const $numberButtons = document.querySelector(".number");           //define variables
-    const $operatorButtons = document.querySelectorAll (".operator, .clear");
-    const $equalButton = document.querySelector(".equal-sign");
-    const calculation = [];
+
+//alert("hello");
+const textButtons = document.querySelectorAll(".number");
+const textOperator = document.querySelectorAll(".operator, .clear");
+const textCalculate = document.querySelector(".equal-sign");
+const calculation = [];
+
+
+function pushNumber(button) {
+    alert(this.innerHTML);
+    calculation.push(this.innerHTML);
     
-    //console.dir($numberButtons);
-    
-    
-    
-    function pushNumber(event) {                                        //create functions
-       // alert(event.target.value);
-       calculation.push(event.target.value);
-    }
-    
-    
-    function pushOperator(event) {
-           // alert(event.target.value);
-            calculation.push(event.target.value); //every time a button is pushed, value is added into calculation array
-     }
-    
-    function calculate() {
+};
+
+function pushOperator(button) {
+    alert(this.innerHTML)
+    calculation.push(this.innerHTML);
+    //console.log(calculation);
+};
+
+
+function calculate() {
     //alert("=");
     let num1 = "",                  //declare as empty string
         num2 = "",
@@ -35,9 +34,9 @@
                 num1 += char;
             } else {
                 num2 += char;
-            }
+            };
     
-        }
+        };
     
         num1 = parseFloat(num1);
         num2 = parseFloat(num2);
@@ -52,56 +51,19 @@
         alert(num1 / num2);
     } else {
             alert("Uh, oh! Something went wrong!");
-        }
-    }
-    
-    
-                     // num1 += calculation[i];
-    
-            
-            
-        
-    
-    
-    
-    
-    
-    //console.log("event", event);
-    //console.log("event target", event.target);
-    //console.log("event target value", event.target.value);
-    //consoel.log("this, this");
-    
-    
-    
-    
-    
-    // example using loop...
-    
-    //      $numberButton.addEventListener("click", pushNumber);
-    
-    //          for (let i = 0; i < $numberButtons.length; i++) { //let i start at zero, run as long as i < number of buttons length, and add 1 every time loop is run
-    //           console.log($numberButtons[i]);
-    //}
-    
-    
-    $numberButtons.forEach(function (button) {                //step 1 make buttons alert pop-up 
-        button.addEventListener("click", pushNumber);          //"button" lets eventListener know you're targeting (button) parameter 
-        //console.log(button);                                  //use for each to loop through node list 
-    });
-    
-    $operatorButtons.forEach(function (button){
-        button.addEventListener("click", pushNumber);
-    });
-    
-    
-    $operatorButtons.forEach(function (button){
-        button.addEventListener("click", pushNumber);
-    });
-    
-    $equalButton.addEventListener("click", calculate);      //we dont need forEach because only one target
-    
-    
-    
-    
-    })();
-    
+        };
+    };
+
+
+ textButtons.forEach((button) => {
+    button.addEventListener("click",pushNumber) 
+});
+
+textOperator.forEach((button) => {
+    button.addEventListener("click",pushOperator) 
+});
+
+
+textCalculate.addEventListener("click", calculate);
+
+
